4a4c99b381c6e97270ddf85da97c822a7975b734
C. Titus Brown <titus@idyll.org>
2020-10-27 21:31:26 -0700

    got some basic tests working

---
+++ tests/pytest_utils.py
@@ -0,0 +1,10 @@
+import os.path
+
+def relative_file(filename):
+    "Return the filename relative to the top level directory of this repo."
+    thisdir = os.path.dirname(__file__)
+    pkgdir = os.path.join(thisdir, "..")
+    newpath = os.path.join(pkgdir, filename)
+    return os.path.abspath(newpath)
+
+
