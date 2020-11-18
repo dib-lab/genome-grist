90751e7898fcaf27d1e3ae6f8b2ca82dbb1a94b5
C. Titus Brown <titus@idyll.org>
2020-11-18 05:38:49 -0800

    ksize, moltype, and scaled handling

--- genome_grist/prefetch_gather.py
+++ genome_grist/prefetch_gather.py
@@ -23,39 +23,51 @@ def main():
     p.add_argument("--save-matches", required=True)
     p.add_argument("--output-unassigned")
     p.add_argument("--threshold-bp", type=float, default=1e5)
+    p.add_argument("-k", "--ksize", type=int, default=31)
+    p.add_argument("--moltype", default="dna")
     args = p.parse_args()
 
     # flatten --db and --query lists
     args.db = [item for sublist in args.db for item in sublist]
     args.query = [item for sublist in args.query for item in sublist]
 
+    ksize = args.ksize
+    moltype = args.moltype
+
     # build one big query:
     query_sigs = []
     for query_file in args.query:
-        sigs = sourmash_args.load_file_as_signatures(query_file, ksize=31)
+        sigs = sourmash_args.load_file_as_signatures(query_file, ksize=ksize,
+                                                     select_moltype=moltype)
         query_sigs.extend(sigs)
 
     mh = query_sigs[0].minhash
     for query_sig in query_sigs[1:]:
         mh += query_sig.minhash
 
+    if not mh.scaled:
+        notify(f"ERROR: must use scaled signatures.")
+        sys.exit(-1)
+
     unident_mh = copy.copy(mh)
 
     notify(f"Loaded {len(mh.hashes)} hashes from "
-           "{len(query_sigs)} query signatures.")
+           f"{len(query_sigs)} query signatures.")
 
     # iterate over signatures in one at a time
     keep = []
     n = 0
     for db in args.db:
-        for sig in sourmash_args.load_file_as_signatures(db, ksize=31):
+        for sig in sourmash_args.load_file_as_signatures(db, ksize=ksize,
+                                                      select_moltype=moltype):
             n += 1
-            common = mh.count_common(sig.minhash)
+            db_mh = sig.minhash.downsample(scaled=mh.scaled)
+            common = mh.count_common(db_mh)
             if common:
                 # check scaled...
                 if common * mh.scaled >= args.threshold_bp:
                     keep.append(sig)
-                    unident_mh.remove_many(sig.minhash.hashes)
+                    unident_mh.remove_many(db_mh.hashes)
 
             if n % 10 == 0:
                 notify(f"{n} searched, {len(keep)} matches.", end="\r")
