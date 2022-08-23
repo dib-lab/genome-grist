2a4ed9e968f76b4349814abccbb6f8f50f08dea8
C. Titus Brown <titus@idyll.org>
2022-08-23 07:00:58 -0700

    first pass at a test; still need to verify error

---
+++ tests/test_snakemake_errors.py
@@ -0,0 +1,89 @@
+"Tests snakemake execution via click CLI module."
+import pytest
+import tempfile
+import shutil
+import os
+import yaml
+import sourmash
+import contextlib
+import io
+
+from genome_grist.__main__ import run_snakemake
+from . import pytest_utils as utils
+
+# NOTE re dependencies (@pytest.mark.dependency):
+# - These basically duplicate the snakemake dependencies.
+# - they're there for convenience, because...
+# - ...if these are wrong, the tests will still succeed, they just may
+#   do some extra work in some tests & take longer.
+
+#
+# NOTE re common output directory - all of these tests run with the same
+# output dir.
+#
+
+def setup_module(m):
+    global _tempdir
+    _tempdir = tempfile.mkdtemp(prefix="genome_grist_test")
+
+
+def teardown_module(m):
+    global _tempdir
+    try:
+        shutil.rmtree(_tempdir, ignore_errors=True)
+    except OSError:
+        pass
+
+
+@pytest.mark.dependency()
+def test_smash_sig():
+    # run 'smash_reads'
+    global _tempdir
+
+    abundtrim_dir = os.path.join(_tempdir, "abundtrim")
+    os.mkdir(abundtrim_dir)
+
+    conf = utils.relative_file('tests/test-data/SRR5950647_subset-nomatches.conf')
+    src = utils.relative_file("tests/test-data/SRR5950647_subset.abundtrim.fq.gz")
+    shutil.copy(src, abundtrim_dir)
+
+    extra_args = ["smash_reads"]
+    status = run_snakemake(
+        conf,
+        verbose=True,
+        outdir=_tempdir,
+        extra_args=extra_args,
+    )
+    assert status == 0
+
+    output_sig = f"{_tempdir}/sigs/SRR5950647_subset.abundtrim.sig.zip"
+    assert os.path.exists(output_sig)
+    sigs = list(sourmash.load_file_as_signatures(output_sig))
+    assert len(sigs) == 3
+    for s in sigs:
+        assert s.minhash.track_abundance
+
+
+@pytest.mark.dependency(depends=["test_smash_sig"])
+def test_gather_reads_nomatches():
+    # run gather_reads, => no matches - should provide error.
+    global _tempdir
+
+    conf = utils.relative_file('tests/test-data/SRR5950647_subset-nomatches.conf')
+    extra_args = ["gather_reads"]
+
+    with contextlib.redirect_stdout(io.StringIO()) as err_log_fp:
+        status = run_snakemake(
+            conf,
+            verbose=True,
+            outdir=_tempdir,
+            extra_args=extra_args,
+        )
+
+    assert status != 0
+
+    #err_log = err_log_fp.getvalue()
+    #print('XXX', (err_log,), 'XYZ')
+
+    #assert "DB is tests/test-data/SRR5950647-genomes/acidulo.zip" in err_log
+    #assert "** ERROR: prefetch didn't find anything for sample 'SRR5950647_subset'." in err_log
