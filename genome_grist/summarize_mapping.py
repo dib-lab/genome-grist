1e4fee0d94ee2d9bb86122d3ca85cc2029918dc3
C. Titus Brown <titus@idyll.org>
2022-09-17 10:35:48 -0700

    [MRG] change column names in mapping summary files; add count of mapped reads. (#209)
    * add count_mapped_reads.txt
    
    * add n_mapped_reads to summary file
    
    * update mapping notebook to reflect new columns
    
    * cleanup calculations
    
    * more cleanup
    
    * add outputs for test to repo
    
    * update to sourmash v4.5.0
    
    * whups, fix f_missed_bp to be a fraction not a percentage
    
    * updated summary CSVs
    
    * update notebook fig 1

--- genome_grist/summarize_mapping.py
+++ genome_grist/summarize_mapping.py
@@ -43,6 +43,11 @@ def summarize_vcf(vcf_gz):
 def main():
     p = argparse.ArgumentParser()
     p.add_argument('sample_name')
+
+    # take in multiple depth.txt files and then below we figure out
+    # the names of the vcf.gz and count_mapped_reads.txt files.
+    # this is a little clunky but seems to be the simplest way to
+    # avoid complicated argument passing.
     p.add_argument('depth_txts', nargs='+',
                    help='output of samtools depth -aa {bamfile}')
     p.add_argument('-o', '--output', required=True, help='output CSV file')
@@ -51,11 +56,23 @@ def main():
     sample = args.sample_name
     runs = {}
     for n, depth_txt in enumerate(args.depth_txts):
+        # figure out the names/paths of the other required files -
         assert depth_txt.endswith('.depth.txt')
-        vcf_gz = depth_txt[:-len('.depth.txt')] + '.vcf.gz'
+        prefix = depth_txt[:-len('.depth.txt')]
+        vcf_gz = prefix + '.vcf.gz'
         assert os.path.exists(vcf_gz)
+        mapcount = prefix + '.count_mapped_reads.txt'
+        assert os.path.exists(mapcount)
+
         print(f"reading from '{vcf_gz}'", file=sys.stderr)
         _, n_chrom, n_snps = summarize_vcf(vcf_gz)
+
+        print(f"reading from '{mapcount}", file=sys.stderr)
+        with open(mapcount, 'rt') as fp:
+            lines = fp.readlines()
+            assert len(lines) == 1
+            line = lines[0].strip()
+            n_mapped_reads = int(line)
 
         print(f"reading from '{depth_txt}", file=sys.stderr)
 
@@ -71,23 +88,39 @@ def main():
         d['n_snps'] = n_snps
 
         value_counts = data['coverage'].value_counts()
-        d['genome bp'] = int(len(data))
-        d['missed'] = int(value_counts.get(0, 0))
-        d['percent missed'] = 100 * d['missed'] / d['genome bp']
-        d['coverage'] = data['coverage'].sum() / len(data)
-        if d['missed'] != 0:
-            uniq_cov = d['coverage'] / (1 - d['missed'] / d['genome bp'])
-            d['unique_mapped_coverage'] = uniq_cov
-        else:
-            d['unique_mapped_coverage'] = d['coverage']
-        covered_bp = (1 - d['percent missed']/100.0) * d['genome bp']
-        d['covered_bp'] = round(covered_bp + 0.5)
+        d['n_genome_bp'] = int(len(data))
+        d['n_missed_bp'] = int(value_counts.get(0, 0))
+        d['f_missed_bp'] = d['n_missed_bp'] / d['n_genome_bp']
+
+        covered_bp = d['n_genome_bp'] - d['n_missed_bp']
+        d['n_covered_bp'] = covered_bp
+        d['f_covered_bp'] = covered_bp / d['n_genome_bp']
+
+        # average over all (incl uncovered) bases:
+        sum_coverage = data['coverage'].sum()
+        d['avg_coverage'] = sum_coverage / len(data)
+
+        # only average over covered bases
+        d['avg_unique_mapped_coverage'] = sum_coverage / covered_bp
+
+        # store identifier info etc.
         d['genome_id'] = genome_id
         d['sample_id'] = sample
 
+        # track num mapped_reads
+        d['n_mapped_reads'] = n_mapped_reads
+
+        # common sense checks, b/c math is hard
+        assert d['n_missed_bp'] + d['n_covered_bp'] == d['n_genome_bp']
+        assert round(d['f_covered_bp'] + d['f_missed_bp'], 3) == 1
+        if d['n_missed_bp'] == 0:
+            assert d['avg_unique_mapped_coverage'] == d['avg_coverage']
+
+        # store for output.
         runs[genome_id] = d
 
-    pd.DataFrame(runs).T.to_csv(args.output)
+    # convert dictionary into CSV via a pandas DataFrame.
+    pd.DataFrame(runs).T.to_csv(args.output, index_label="index")
     print(f"Wrote CSV to {args.output}", file=sys.stderr)
 
     return 0
